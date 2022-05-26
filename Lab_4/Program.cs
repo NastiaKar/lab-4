1bb353bdbede808540ed6cbd0075976246b4a6d4
NastiaKar <nastia.karpyuk@outlook.com>
2022-05-26 14:20:18 +0300

    first commit

---
+++ Lab_4/Program.cs
@@ -0,0 +1,24 @@
+﻿using static System.Console;
+
+namespace Lab_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Motorcycle mot = new Motorcycle();
+            WriteLine(mot);
+            WriteLine();
+            Scooter sc = new Scooter();
+            WriteLine(sc);
+            WriteLine();
+            Car car = new Car();
+            WriteLine(car);
+            WriteLine();
+            Train tr = new Train();
+            WriteLine(tr);
+            WriteLine();
+            ReadKey();
+        }
+    }
+}
---
+++ Lab_4/Transport.cs
@@ -0,0 +1,80 @@
+﻿using System.Threading.Channels;
+using Lab_4;
+
+namespace Lab_4;
+abstract class Transport
+{
+    public abstract int Wheels { get; }
+    public abstract int Speed { get; }
+    public abstract bool DoorsAvailable { get; }
+    // public abstract void GetWheelsAmount();
+    // public abstract void CheckDoors();
+}
+
+class Motorcycle : Transport
+{
+    private Random rand = new Random();
+    public override int Wheels => 2;
+    public override bool DoorsAvailable => false;
+    public override int Speed { get; }
+    public Motorcycle()
+    {
+        Speed = rand.Next(100, 300);
+    }
+
+    public override string ToString()
+    {
+        return $"Speed: {Speed}\nWheels: {Wheels}\nDoors available: {DoorsAvailable}";
+    }
+}
+
+class Scooter : Transport
+{
+    private Random rand = new Random();
+    public override int Wheels => 2;
+    public override bool DoorsAvailable => false;
+    public override int Speed { get; }
+    public Scooter()
+    {
+        Speed = rand.Next(80, 120);
+    }
+
+    public override string ToString()
+    {
+        return $"Speed: {Speed}\nWheels: {Wheels}\nDoors available: {DoorsAvailable}";
+    }
+}
+
+class Car : Transport
+{
+    private Random rand = new Random();
+    public override int Wheels => 4;
+    public override bool DoorsAvailable => true;
+    public override int Speed { get; }
+    public Car()
+    {
+        Speed = rand.Next(60, 240);
+    }
+
+    public override string ToString()
+    {
+        return $"Speed: {Speed}\nWheels: {Wheels}\nDoors available: {DoorsAvailable}";
+    }
+}
+
+class Train : Transport
+{
+    private Random rand = new Random();
+    public override int Wheels => 64;
+    public override bool DoorsAvailable => true;
+    public override int Speed { get; }
+    public Train()
+    {
+        Speed = rand.Next(100, 400);
+    }
+
+    public override string ToString()
+    {
+        return $"Speed: {Speed}\nWheels: {Wheels}\nDoors available: {DoorsAvailable}";
+    }
+}
